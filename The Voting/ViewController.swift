30fe3e00d1168511a58231a2aae25013b3bf3c21
Student <student@msutton.local>
2016-04-06 11:42:40 -0500

    Initial Commit

---
+++ The Voting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Voting
+//
+//  Created by Maddy Sutton on 4/6/16.
+//  Copyright © 2016 Bits Please. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
