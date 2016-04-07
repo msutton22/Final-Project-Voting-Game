c4f6322bedd2644a59ad2feb6e64fcf47d5ff6fa
rkalvani <student@rkalvani.local>
2016-04-07 12:13:37 -0500

    connected button and label elements to the custom class

--- The Voting/QuestionViewController.swift
+++ The Voting/QuestionViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var playerTurnIdentifierLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func p1NameButton(sender: AnyObject) {
     }
-    */
+    
+    @IBAction func p2NameButton(sender: AnyObject) {
+    }
 
+    @IBAction func p3NameButton(sender: AnyObject) {
+    }
+    
+    @IBAction func p4NameButton(sender: AnyObject) {
+    }
+    
+    @IBAction func p5NameButton(sender: AnyObject) {
+    }
+    
 }
