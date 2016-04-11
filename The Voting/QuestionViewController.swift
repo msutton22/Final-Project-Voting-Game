db0704d112bd2baf7f9c2a286213f61ad98573d7
Student <student@mkelly2.local>
2016-04-11 12:03:40 -0500

    got ride of button colors

--- The Voting/QuestionViewController.swift
+++ The Voting/QuestionViewController.swift
@@ -25,15 +25,11 @@ class QuestionViewController: UIViewController {
         super.viewDidLoad()
         questionLabel.text = String(info.questions)
         player1Button.setTitle(info.player1, forState: UIControlState.Normal)
-            player1Button.titleLabel?.backgroundColor = UIColor.blackColor()
         player2Button.setTitle(info.player2, forState: UIControlState.Normal)
-            player2Button.titleLabel?.backgroundColor = UIColor.blackColor()
         player3Button.setTitle(info.player3, forState: UIControlState.Normal)
-            player3Button.titleLabel?.backgroundColor = UIColor.blackColor()
         player4button.setTitle(info.player4, forState: UIControlState.Normal)
-            player4button.titleLabel?.backgroundColor = UIColor.blackColor()
         player5Button.setTitle(info.player5, forState: UIControlState.Normal)
-            player5Button.titleLabel?.backgroundColor = UIColor.blackColor()
+    
     }
     
     @IBAction func p1NameButton(sender: AnyObject) {
