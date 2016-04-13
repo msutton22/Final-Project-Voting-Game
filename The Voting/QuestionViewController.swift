f935b196956916a8851bebd7ca654a661c105f96
Student <student@msutton.local>
2016-04-13 11:44:41 -0500

    Fixed stuff

--- The Voting/QuestionViewController.swift
+++ The Voting/QuestionViewController.swift
@@ -24,9 +24,11 @@ class QuestionViewController: UIViewController {
     
         override func viewDidLoad() {
         super.viewDidLoad()
-            questionLabel.text = String(info.questions)
-            
-
+   player1Button.setTitle(info.player1, forState: UIControlState.Normal)
+            player2Button.setTitle(info.player2, forState: UIControlState.Normal)
+            player3Button.setTitle(info.player3, forState: UIControlState.Normal)
+        player4button.setTitle(info.player4, forState: UIControlState.Normal)
+            player5Button.setTitle(info.player5, forState: UIControlState.Normal)
     }
     
     @IBAction func p1NameButton(sender: AnyObject) {
