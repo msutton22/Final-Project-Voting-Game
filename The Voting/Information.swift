60771d87d96a3a3e7966e0861ef10b8ae19782ed
Student <student@mkelly2.local>
2016-04-13 12:08:06 -0500

    fixed questions

--- The Voting/Information.swift
+++ The Voting/Information.swift
@@ -15,7 +15,7 @@ class Information: NSObject {
     var player4 = ""
     var player5 = ""
  
-    var questions = ["Who would have the hardest time talking their way out of an insane asylum?", "Who will get convicted of a crime they didn't commit?", "Who has gotten the most upset while playing Monopoly?", "Most Likely To Move Out Of The Country", "Who is most likely to become a reality TV star?", "Who is the biggest flirt?", "Who has the most obnoxious food allergy?", "Who is most likely to put their foot in their mount", "Who is going to have to most annoying kids", "Who is most likely to be late to their own Wedding","Who is the biggest Drama Queen", "Who is the most clumsy", "Who is most likely to be ID'd when they are 30", "Who is most likely to take over the world", "Who is most likely to sit at the kids table at family parties", " Who is most likely to meet a ghost in the bathroom?"]
+    var questions = [" have the hardest time talking their way out of an insane asylum?", "get convicted of a crime they didn't commit?", " get upset while playing Monopoly?", " Move Out Of The Country", " become a reality TV star?", "be the biggest flirt?","put their foot in their mount", " have to most annoying kids", "be late to their own Wedding","be the biggest Drama Queen", "fall down a flight of stairs", "be ID'd when they are 30", "take over the world", "sit at the kids table at family parties", "meet a ghost in the bathroom?"]
 
 
     
