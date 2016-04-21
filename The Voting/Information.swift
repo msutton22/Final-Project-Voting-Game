9c00ac07c503ed1d4a56804b71f025448128103a
Student <student@msutton.local>
2016-04-21 12:27:02 -0500

    added counter to information

--- The Voting/Information.swift
+++ The Voting/Information.swift
@@ -19,7 +19,7 @@ class Information: NSObject {
     var player3won = 0
     var player4won = 0
     var player5won = 0
-    
+    var noPlayerWon = 0
     
  
     var questions = ["have the hardest time talking their way out of an insane asylum?", " get convicted of a crime they didn't commit?", " get upset while playing Monopoly?", " move out of the country?", " become a reality TV star?", " be the biggest flirt?"," put their foot in their mounth", " have to most annoying kids?", " be late to their own wedding?"," be the biggest Drama Queen?", " fall down a flight of stairs?", " be ID'd when they are 30?", " take over the world?", " sit at the kids table at family parties?", " meet a ghost in the bathroom?", "be your one phone call if you got arrested", "be nominated as Mr./Mrs. Sunshine", "drown in a kiddie pool", " go skinny dipping", " get lost in the woods", " try and high five a blind person", " steal candy from a baby", " crash on your couch", " be on SNL", "lose an election because of something they did in high school", "get into a fight with an old lady and lose", "be caught sleeping on the job", "surive the longest in the zombie apocolypse", "cry in public", "drop their phone in a the toilet", "survive the hunger games", "know everysong from one direction", "lose the winning loto ticket", "go on the bachlor/bachlorette","go into public in their pj's", "fall asleep at work/school", "have a first date at a fast food place", "wreck a car", "fail their drivers test multiple times", "scream when they see a bug", "go skydiving"]
