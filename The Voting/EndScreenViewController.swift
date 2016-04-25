93bb9c18ccf09949acdb04622d21fb06b4277c7c
rkalvani <student@rkalvani.local>
2016-04-25 12:44:16 -0500

    figured out morgan forgot the SEGUE!! MMMMOOOORRRRRGGGAAAAAANNNNNNNNN

--- The Voting/EndScreenViewController.swift
+++ The Voting/EndScreenViewController.swift
@@ -28,22 +28,22 @@ class EndScreenViewController: UIViewController {
     func overallWinner() {
         if info.noPlayerWon > (info.player1won + info.player2won + info.player3won + info.player4won + info.player5won)
         {
-            endScreenLabel.text = "No one was thrown under the bus"
+            endScreenLabel.text = "It's a tie. No one was thrown under the bus."
         }
-        else if info.player1won  > (info.player4won + info.player2won +  info.player3won + info.player5won) {
-            endScreenLabel.text = "\(info.player1) is the person that is most likely to be thrown under the bus"
+        if info.player1won > (info.player4won + info.player2won +  info.player3won + info.player5won) {
+            endScreenLabel.text = "\(info.player1) was thrown under the bus"
         }
-        else if info.player2won  > (info.player1won + info.player4won + info.player3won + info.player5won) {
-            endScreenLabel.text = "\(info.player2) is the person that is most likely to be thrown under the bus"
+        if info.player2won > (info.player1won + info.player4won + info.player3won + info.player5won) {
+            endScreenLabel.text = "\(info.player2) was thrown under the bus"
         }
-        else if info.player3won > (info.player1won + info.player2won + info.player4won + info.player5won) {
-            endScreenLabel.text = "\(info.player3)is the person that is most likely to be thrown under the bus"
+        if info.player3won > (info.player1won + info.player2won + info.player4won + info.player5won) {
+            endScreenLabel.text = "\(info.player3) was thrown under the bus"
         }
-        else if info.player4won > (info.player1won + info.player2won + info.player3won + info.player5won) {
-            endScreenLabel.text = "\(info.player4) is the person that is most likely to be thrown under the bus"
+        if info.player4won > (info.player1won + info.player2won + info.player3won + info.player5won) {
+            endScreenLabel.text = "\(info.player4) was thrown under the bus"
         }
-        else if info.player5won  > (info.player1won + info.player2won + info.player3won + info.player4won) {
-            endScreenLabel.text = "\(info.player5) is the person that is most likely to be thrown under the bus"
+        if info.player5won > (info.player1won + info.player2won + info.player3won + info.player4won) {
+            endScreenLabel.text = "\(info.player5) was thrown under the bus"
         }
     }
 }
