513a7cf539a4672fd902355a258d0e2c09ed975e
rkalvani <student@rkalvani.local>
2016-04-25 12:10:30 -0500

    end screen messages have been improved

--- The Voting/EndScreenViewController.swift
+++ The Voting/EndScreenViewController.swift
@@ -27,49 +27,50 @@ class EndScreenViewController: UIViewController {
     
     func overallWinner() {
         if info.player1won == info.player2won && (info.player1won * 3) > (info.player3won + info.player4won + info.player5won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
         else if info.player1won == info.player3won && (info.player1won * 3) > (info.player2won + info.player4won + info.player5won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown someone under the bus"
         }
         else if info.player1won == info.player4won && (info.player1won * 3) > (info.player2won + info.player3won + info.player5won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
         else if info.player1won == info.player5won && (info.player1won * 3) > (info.player2won + info.player3won + info.player4won){
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"        }
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
+        }
         else if info.player2won == info.player3won && (info.player2won * 3) > (info.player1won + info.player4won + info.player5won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
         else if info.player2won == info.player4won && (info.player2won * 3) > (info.player1won + info.player3won + info.player5won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
         else if info.player2won == info.player5won && (info.player2won * 3) > (info.player1won + info.player4won + info.player3won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
         else if info.player3won == info.player4won && (info.player3won * 3) > (info.player1won + info.player2won + info.player5won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
         else if info.player3won == info.player5won && (info.player3won * 3) > (info.player1won + info.player4won + info.player2won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
         else if info.player4won == info.player5won && (info.player4won * 3) > (info.player1won + info.player2won + info.player3won) {
-            endScreenLabel.text = "Its a tie. No one wanted to throw someone under the bus"
+            endScreenLabel.text = "Its a tie. No one was thrown under the bus"
         }
             
         else if (info.player1won * 4) > (info.player4won + info.player2won + info.player3won + info.player5won) {
-            endScreenLabel.text = "Player 1 is the person that is most likely to be thrown under the bus"
+            endScreenLabel.text = " \(info.player1) was thrown under the bus"
         }
         else if (info.player2won * 4) > (info.player1won + info.player4won + info.player3won + info.player5won) {
-            endScreenLabel.text = "Player 2 is the person that is most likely to be thrown under the bus"
+            endScreenLabel.text = " \(info.player2) was thrown under the bus"
         }
         else if (info.player3won * 4) > (info.player1won + info.player2won + info.player4won + info.player5won) {
-            endScreenLabel.text = "Player 3 is the person that is most likely to be thrown under the bus"
+            endScreenLabel.text = " \(info.player3) was thrown under the bus"
         }
         else if (info.player4won * 4) > (info.player1won + info.player2won + info.player3won + info.player5won) {
-            endScreenLabel.text = "Player 4 is the person that is most likely to be thrown under the bus"
+            endScreenLabel.text = " \(info.player4) was thrown under the bus"
         }
         else if (info.player5won * 4) > (info.player1won + info.player2won + info.player3won + info.player4won) {
-            endScreenLabel.text = "Player 5 is the person that is most likely to be thrown under the bus"
+            endScreenLabel.text = " \(info.player5) was thrown under the bus"
         }
     }
 
